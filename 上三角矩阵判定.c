7dbb61d28f0c6aacece060e8d226050c743d73f3
Common-Graduate <2968799534@qq.com>
2022-08-05 15:07:19 +0800

    //上三角矩阵即主对角线以下的元素都为0的矩阵，主对角线为从矩阵的左上角至右下角的连线。

---
+++ 上三角矩阵判定.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+//ÉÏÈı½Ç¾ØÕóÅĞ¶¨
+int main()
+{
+    int n = 0;
+    scanf("%d", &n);
+    int arr[10][10] = { 0 };
+    int i = 0, j = 0;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    int flag = 1;
+    for (i = 1; i < n; i++)
+    {
+        for (j = 0; j < i; j++)
+        {
+            if (arr[i][j] != 0)
+            {
+                flag = 0;
+                break;
+            }
+        }
+        if (flag == 0)
+            break;
+    }
+    if (flag)
+        printf("YES\n");
+    else
+        printf("NO\n");
+    return 0;
+}
